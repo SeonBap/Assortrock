87c481d471254b263d98b06bf8ad376e7e921018
SeonBap <tjsrnr3180@gmail.com>
2023-05-22 16:04:59 +0900

    숙제 0522 완료

--- Homework/0522/0522.cpp
+++ Homework/0522/0522.cpp
@@ -12,9 +12,14 @@
 // 그냥 무식하게 생각해라.
 
 // 숙제 3 -- 누군가 죽었다. 누군가의 Hp가 0이 된순간 종료.
-
 // 몬스터와 플레이어가 서로 한대씩 때리면서
 // 한명이 죽으면 끝납니다.
+// if (조건 만족시)
+// {
+//     return;
+// }
+
+
 
 void StatusRender(const char* const _Name, int _Att, int _Hp)
 {
@@ -53,47 +58,62 @@ void DamagePrint(const char* const _AttName, const char* const _DefName, int _At
 void Damage(int& _Hp, int _Att)
 {
     _Hp -= _Att;
+
+    if (_Hp < 0)
+    {
+        _Hp = 0;
+    }
 }
 
 int main()
 {
     int PlayerHp = 200;
-    int PlayerAtt = 100;
+    int PlayerAtt = 40;
     char PlayerName[40] = "Player";
 
     int MonsterHp = 100;
     int MonsterAtt = 10;
     char MonsterName[40] = "Monster";
 
+    StatusRender(PlayerName, PlayerAtt, PlayerHp);
+    StatusRender(MonsterName, MonsterAtt, MonsterHp);
+
+    // 아무키나 입력할때까지 기다리는 함수
+    _getch();
+
+
     while (true)
     {
         // 콘솔창을 모조리 지우는 함수
         system("cls");
 
-        // 한번에 함수로 만든것이고
+        Damage(MonsterHp, PlayerAtt);
+        
         StatusRender(PlayerName, PlayerAtt, PlayerHp);
         StatusRender(MonsterName, MonsterAtt, MonsterHp);
-
-        _getch();
-
-        // 아무키나 입력할때까지 기다리는 함수
-
         DamagePrint(PlayerName, MonsterName, PlayerAtt);
-        Damage(MonsterHp, PlayerAtt);
         // MonsterHp -= PlayerAtt;
 
+        if (MonsterHp == 0)
+        {
+            return 0;
+        }
+
         _getch();
 
-        DamagePrint(MonsterName, PlayerName, MonsterAtt);
+        system("cls");
+
         Damage(PlayerHp, MonsterAtt);
 
+        StatusRender(PlayerName, PlayerAtt, PlayerHp);
+        StatusRender(MonsterName, MonsterAtt, MonsterHp);
+        DamagePrint(MonsterName, PlayerName, MonsterAtt);
+
+        if (PlayerHp == 0)
+        {
+            return 0;
+        }
+
         _getch();
-
-        //if (조건 만족시)
-        //{
-        //    return;
-        //}
-
-
     }
 }
