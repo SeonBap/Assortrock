4f207cd7c899f0eee573e7455ee99458b9da877e
SeonBap <tjsrnr3180@gmail.com>
2023-06-19 12:53:54 +0900

    강의 0619

---
+++ Ar_Unreal/Source/Ar_Unreal/Snake/SnakeHead.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SnakeHead.h"
+
+// Sets default values
+ASnakeHead::ASnakeHead()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASnakeHead::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASnakeHead::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
